189fdb063fccf222e42b48a0515f25e83ec7d3f5
AndroShivam <52043419+AndroShivam@users.noreply.github.com>
2020-12-13 09:40:38 +0530

    Create Rotate Image (Rotate Matrix Clockwise).java

---
+++ Array/Rotate Image (Rotate Matrix Clockwise).java
@@ -0,0 +1,33 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        int n = matrix.length;
+        
+        // transponse matrix
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        // swap first and last element of row
+        for(int i=0; i<n; i++){
+            for(int j=0; j<(n/2); j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+        
+    }
+}
+
+/*
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+*/
