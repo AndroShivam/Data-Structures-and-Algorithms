0cf208a7c9bbc71bf2aec31564dd9964ed103bf4
AndroShivam <shivi08567@gmail.com>
2021-01-04 19:45:07 +0530

    Create Longest Substring Without Repeating Characters.java

---
+++ String/Longest Substring Without Repeating Characters.java
@@ -0,0 +1,75 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        
+        int n = s.length();
+        int ans = 0;
+        
+       for(int i=0; i<n; i++)
+           for(int j=i+1; i<=n; j++)
+               if(allUnique(s,i,j))
+                   ans = Math.max(ans, j - i);
+        
+        return ans;
+        
+    }
+    
+    private boolean allUnique(String s, int start, int end){
+        Set<Character> set = new HashSet<>();
+        for(int i=start; i<end; i++){
+            char ch = s.charAt(i);
+            if(set.contains(ch))
+                return false;
+            set.add(ch);
+        }
+        
+        return true;
+    }
+}
+
+
+
+
+// Time Complexity - Time complexity : O(2n) = O(n)O(2n)=O(n). In the worst case each character will be visited twice by ii and jj.
+// Sliding Window
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
+
+
+// Above Approach further more optimized
+// Time complexity : O(n)O(n). Index jj will iterate nn times.
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length() == 0)
+            return 0;
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        
+        for(int i=0,j=0; i<s.length(); i++){
+            if(map.containsKey(s.charAt(i))){
+                j = Math.max(j, map.get(s.charAt(i)) + 1);
+            }
+            
+            map.put(s.charAt(i),i);
+            max = Math.max(max, i - j + 1);
+        }
+        
+        return max;
+    }
+}
