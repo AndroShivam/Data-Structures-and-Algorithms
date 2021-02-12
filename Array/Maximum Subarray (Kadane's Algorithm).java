bd9e6cb9b3a757cbd0df35a6e1bd5e8ac9aa0dc9
Shivam Sharma <shivi08567@gmail.com>
2021-02-12 18:54:33 +0530

    Update Maximum Subarray (Kadane's Algorithm)

--- Array/Maximum Subarray (Kadane's Algorithm).java
+++ Array/Maximum Subarray (Kadane's Algorithm).java
@@ -1,3 +1,27 @@
+// Brute Force O(N^2)
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int maxValue = 0;
+        
+        for(int i=0; i<nums.length; i++){
+            int currentMax = 0;
+            
+            for(int j=i; j<nums.length; j++){
+                currentMax += nums[j];
+                if(currentMax > maxValue){
+                    maxValue = currentMax;
+                }
+            }
+        }
+        
+        return maxValue;
+    }
+}
+
+// Kadane's Algorithm O(N)
+ 
 class Solution {
     public int maxSubArray(int[] nums) {
         
