e8cdc8acefea7c87467a1705955e1430503d30f4
Shivam Sharma <shivi08567@gmail.com>
2021-02-18 18:30:53 +0530

    Create Pascal's Triangle

---
+++ Array/Pascal's Triangle.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> row , pre = null;
+        
+        for(int i=0; i<numRows; i++){
+            row = new ArrayList<>();
+            for(int j=0; j<=i; j++){
+                if(j == 0 || j == i) // if j is either first or last, add 1 
+                    row.add(1);
+                else
+                    row.add(pre.get(j - 1) + pre.get(j));
+            }
+            pre = row;
+            result.add(row);
+        }
+        
+        return result;
+    }
+}
