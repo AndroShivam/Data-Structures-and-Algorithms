117b59306e0a625c57f0e925c5a74e7b8403b41a
Shivam Sharma <shivi08567@gmail.com>
2021-02-19 10:13:38 +0530

    Create Binary Tree Inorder Traversal (Iterative)

---
+++ Tree/Binary Tree Inorder Traversal (Iterative).java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        
+        while(current != null || !stack.isEmpty()){
+            while(current != null){
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+        
+        return result;
+    }
+}
