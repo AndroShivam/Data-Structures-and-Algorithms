1d2a54333c5b4e99c78cd96bade07e4def6d3605
Shivam Sharma <shivi08567@gmail.com>
2021-02-20 10:22:59 +0530

    Update Remove Duplicates from Sorted Array

--- Array/Remove Duplicates from Sorted Array.java
+++ Array/Remove Duplicates from Sorted Array.java
@@ -1,15 +1,47 @@
+// Time Complexity - O(N)
+// Space Complexity - O(N)
+
 class Solution {
     public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if(n < 2) return n;
         
-        int count = 0;
+        int[] temp = new int[n];
+        int j = 0;
         
-        for(int i=1; i<nums.length; i++){
-            if(nums[i] == nums[i - 1])
-                count++;
-            else
-                nums[i - count] = nums[i];
+        for(int i=0; i<n-1; i++){
+            if(nums[i] != nums[i + 1])
+                temp[j++] = nums[i];
         }
         
-        return nums.length - count;
+        temp[j++] = nums[n - 1];
+        
+        for(int i=0; i<j; i++)
+            nums[i] = temp[i];
+        
+        return j;
+    }
+}
+
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if(n < 2) return n;
+        
+        int j = 0;
+        
+        for(int i=0; i<n-1; i++){
+            if(nums[i] != nums[i + 1])
+                nums[j++] = nums[i];
+        }
+         
+        // Store the last element as whether 
+        // it is unique or repeated, it hasn't 
+        // stored previously 
+        nums[j++] = nums[n - 1];
+        return j;
     }
 }
