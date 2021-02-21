a35b4adf6538021d8edba925a5cc5c855293f309
Shivam Sharma <shivi08567@gmail.com>
2021-02-21 12:31:09 +0530

    Update Reverse Nodes in k-Group

--- Linked List/Reverse Nodes in k-Group.java
+++ Linked List/Reverse Nodes in k-Group.java
@@ -1,21 +1,11 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         if(head == null || head.next == null || k == 1)
             return head;
         
-        ListNode dummyHead = new ListNode(0);
-        dummyHead.next = head;
-        ListNode begin = dummyHead;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode begin = dummy;
         
         int i = 0;
         
@@ -29,42 +19,91 @@ class Solution {
             }
         }
         
-        return dummyHead.next;
+        return dummy.next;
     }
     
+    
     private ListNode reverse(ListNode begin, ListNode end){
-        ListNode curr = begin.next;
-        ListNode next,first;
-        ListNode prev = begin;
-        first = curr;
+        ListNode current = begin.next;
+        ListNode prev = begin, next = null;
+        ListNode first = current;
         
-        while(curr != end){
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+        while(current != end){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
         }
         
         begin.next = prev;
-        first.next = curr;
+        first.next = current;
+        
         return first;
     }
 }
 
+/*
 
-/**
- * Reverse a link list between begin and end exclusively
- * an example:
- * a linked list:
- * 0->1->2->3->4->5->6
- * |           |   
- * begin       end
- * after call begin = reverse(begin, end)
- * 
- * 0->3->2->1->4->5->6
- *          |  |
- *      begin end
- * @return the reversed list's 'begin' node, which is the precedence of node end
- */
+100  200  300  400  500  600
+-1 -> 1 -> 2 -> 3 -> 4 -> 5
 
-// credits to @yellowstone on LeetCode
+k = 2
+
+begin = rev(100, 400)
+
+current = 200
+prev = 100
+first = 200
+
+while(current != end) // 200 != 400
+
+next = 300
+current.next = 100
+prev = 200
+current = 300
+
+         current
+100  200  300  400  500  600
+-1 <- 1    2 -> 3 -> 4 -> 5
+
+while(current != end) // 300 != 400
+
+next = 400
+current.next = 200
+prev = 300
+current = 400
+
+              current
+100  200  300  400  500  600
+-1 <- 1 <- 2   3 -> 4 -> 5
+
+while(current != end) // 400 != 400 (false)
+
+Actual representation of linked list
+
+prev first begin    curr
+300  200  100      400  500  600
+2 -> 1 -> -1        3 -> 4 -> 5
+
+begin.next = prev // 300
+
+prev first begin    curr
+300  200  100      400  500  600
+2 -> 1 -> -1        3 -> 4 -> 5
+
+begin prev first    curr
+100  300    200     400  500  600  
+-1 -> 2 ->  1       3 -> 4 -> 5
+
+
+// connect the reversed linked list with remaining linked list
+
+first.next = curr
+
+begin prev first    curr
+100  300    200     400  500  600  
+-1 -> 2 ->  1   ->   3 -> 4 -> 5
+
+return first // end of reversed linkedlist
+ 
+*/
