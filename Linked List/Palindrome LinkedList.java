047205dc3a17df564243de88a1e450671c227fe9
Shivam Sharma <shivi08567@gmail.com>
2021-02-21 19:45:01 +0530

    Update Palindrome LinkedList

--- Linked List/Palindrome LinkedList.java
+++ Linked List/Palindrome LinkedList.java
@@ -1,13 +1,39 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// Time Complexity - O(N)
+// Space Complexity - O(N)
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+     Stack<Integer> stack = new Stack<>();
+        Node slow = head;
+        Node fast = head;
+
+        // push elements till middle
+        while (fast != null && fast.next != null) {
+            stack.push(slow.val);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // in case of odd number of elements, skip middle
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        while (slow != null) {
+            if (stack.pop() != slow.val) {
+                return false;
+            }
+            slow = slow.next;
+        }
+
+        return true;
+      }
+}
+
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+// Note: this approach modifies original linkedlist
+
 class Solution {
     public boolean isPalindrome(ListNode head) {
         
