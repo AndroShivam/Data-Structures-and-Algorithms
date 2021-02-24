dcb01f4822aaf0089c293c8d75aba549d7ce37d2
Shivam Sharma <shivi08567@gmail.com>
2021-02-24 11:56:07 +0530

    Create Count Inversions in an Array

--- Array/Count Inversions in an Array.java
+++ Array/Count Inversions in an Array.java
@@ -16,3 +16,62 @@ class LearningDS{
         System.out.println(inversionCount);
     }
 }
+
+// Time Complexity - O(NLogN)
+// Space Complexity - O(N)
+
+class LearningDS {
+    public static void main(String[] args) {
+        int[] nums = {1, 9, 6, 4, 5};
+
+        int inversionCount = mergeSort(nums, 0, nums.length - 1);
+        System.out.println(inversionCount);
+    }
+
+    private static int mergeSort(int[] nums, int low, int high) {
+        int inversionCount = 0;
+        if (low < high) {
+            int mid = low + (high - low)/2;
+            inversionCount += mergeSort(nums, low, mid);
+            inversionCount += mergeSort(nums, mid + 1, high);
+            inversionCount += merging(nums, low, mid, high);
+        }
+
+        return inversionCount;
+    }
+
+    private static int merging(int[] nums, int low, int mid, int high) {
+
+        int[] temp = new int[nums.length];
+
+        int i = low;
+        int j = mid + 1;
+        int k = low;
+        int inversionCount = 0;
+
+        while (i <= mid && j <= high) {
+            if (nums[i] <= nums[j]) {
+                temp[k] = nums[i];
+                i++;
+            } else {
+                temp[k] = nums[j];
+                j++;
+                inversionCount += (mid - i + 1);
+            }
+            k++;
+        }
+
+        if (i > mid) {
+            while (j <= high)
+                temp[k++] = nums[j++];
+        } else {
+            while (i <= mid)
+                temp[k++] = nums[i++];
+        }
+
+        for(i=low; i<=high; i++)
+            nums[i] = temp[i];
+
+        return inversionCount;
+    }
+}
