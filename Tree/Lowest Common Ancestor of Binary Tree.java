3137bf212f3ddc1495ee9cc22f784e4efbc5d05e
Shivam Sharma <shivi08567@gmail.com>
2021-03-07 13:15:34 +0530

    Create Lowest Common Ancestor of Binary Tree

---
+++ Tree/Lowest Common Ancestor of Binary Tree.java
@@ -0,0 +1,13 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q)
+            return root;
+        
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if(left != null && right != null) return root; // found LCA
+        
+        return left != null ? left : right;
+    }
+}
