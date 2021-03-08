bfac27dd7fd4ce57d7fe94e2d0ad11b2a0e59c01
Shivam Sharma <shivi08567@gmail.com>
2021-03-08 09:13:56 +0530

    Update Binary Tree ZigZag Level Traversal

--- Tree/Binary Tree ZigZag Level Traversal.java
+++ Tree/Binary Tree ZigZag Level Traversal.java
@@ -1,41 +1,34 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        traverse(root, result, 0);
-        return result;
-    }
-    
-    private void traverse(TreeNode root, List<List<Integer>> result, int level){
-        if(root == null) 
-            return;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> row = null;
         
-        if(result.size() <= level){
-            List<Integer> newLevel = new LinkedList<>();
-            result.add(newLevel);
+        if(root == null) return res;
+         
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        boolean zigzag = false;
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            row = new ArrayList<>();
+            for(int i=1; i<=size; i++){
+                TreeNode current = queue.poll();
+                
+                if(zigzag){
+                    row.add(0,current.val);
+                }else{
+                    row.add(current.val);
+                }
+                
+                if(current.left != null) queue.offer(current.left);
+                if(current.right != null) queue.offer(current.right);
+            }
+            res.add(row);
+            zigzag = !zigzag;
         }
         
-        List<Integer> collection = result.get(level);
-        if(level % 2 == 0)
-            collection.add(root.val);
-        else
-            collection.add(0, root.val);
-        
-        traverse(root.left, result, level + 1);
-        traverse(root.right, result, level + 1);
+        return res;
     }
 }
