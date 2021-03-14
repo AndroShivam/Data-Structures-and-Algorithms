953ef941ac1841e04bc6cc27e5ffddf21133086c
Shivam Sharma <shivi08567@gmail.com>
2021-03-14 11:06:16 +0530

    Update Populating Next Right Pointers in each Node

--- Tree/Populating Next Right Pointers in each Node.java
+++ Tree/Populating Next Right Pointers in each Node.java
@@ -41,3 +41,43 @@ class Solution {
         return root;
     }
 }
+
+// if next doesnot point to null initially
+
+class Solution
+{
+    /*
+    Node class is Defined as follows:
+    class Node{
+        int data;
+        Node left;
+        Node right;
+        Node nextRight;
+        Node(int data){
+            this.data = data;
+            left=null;
+            right=null;
+            nextRight = null;
+        }
+    }
+    */
+    public void connect(Node root)
+    {
+        // Your code goes here.
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        queue.offer(null);
+        
+        while(!queue.isEmpty()){
+            Node current = queue.poll();
+            
+            if(current != null){
+                current.nextRight = queue.peek();
+                if(current.left != null) queue.offer(current.left);
+                if(current.right != null) queue.offer(current.right);
+            }else if(!queue.isEmpty()){
+                queue.offer(null);
+            }
+        }
+    }
+}
