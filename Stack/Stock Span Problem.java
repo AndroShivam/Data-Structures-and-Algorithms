55b8f79dd3b528a76e6f1fbf07f8ab05966968fe
Shivam Sharma <shivi08567@gmail.com>
2021-03-19 14:11:32 +0530

    Create Stock Span Problem

--- Stack/Stock Span Problem.java
+++ Stack/Stock Span Problem.java
@@ -1,45 +1,12 @@
-import java.util.Arrays;
-import java.util.Stack;
+// Time Complexity - O(N^2)
 
 class LearningDS {
     public static void main(String[] args) {
-
-        /* 100 80 60 70 60 75 85
-           select a number and scan on its left side increase count till we get number bigger than it
-           there's nothing in the left of 100 so its span is 0
-           80 >= 100 is false so span is 1
-           60 >= 80 is false so span is 1
-           70 >= 60 so span is 2
-           85 > 75 > 60 > 70 > 60 > 80 hence span is 6
-        */
 
         int[] prices = {100, 80, 60, 70, 60, 75, 85};
         calculateSpan(prices, prices.length);
 
         calculateSpanAlt(prices, prices.length);
-    }
-
-    private static void calculateSpanAlt(int[] prices, int n) {
-        Stack<Integer> stack = new Stack<>();
-        int[] span = new int[n];
-        int count = 1;
-
-        for (int i = 0; i < n; i++) {
-            if (stack.isEmpty()) {
-                span[i] = 1;
-            } else if (stack.peek() > prices[i])
-                span[i] = 1;
-            else {
-                while (!stack.isEmpty() && stack.peek() < prices[i]) {
-                    stack.pop();
-                    count++;
-                }
-                span[i] = count;
-            }
-            stack.push(prices[i]);
-        }
-
-        System.out.println(Arrays.toString(span));
     }
 
     private static void calculateSpan(int[] prices, int n) {
@@ -55,5 +22,30 @@ class LearningDS {
     }
 }
 
-// Output
-//[1, 1, 1, 2, 1, 4, 6]
+// Time Complexity - O(N)
+
+class LearningDS {
+    public static void main(String[] args) {
+
+        int[] prices = {100, 80, 60, 70, 60, 75, 85};
+        int[] span = new int[prices.length];
+
+        calculateSpan(prices, prices.length, span);
+    }
+
+    private static void calculateSpan(int[] prices, int n, int[] span) {
+        Stack<Integer> stack = new Stack<>();
+
+        span[0] = 1;
+        stack.push(0);
+
+        for (int i = 1; i < n; i++) {
+            while (!stack.isEmpty() && prices[i] >= prices[stack.peek()])
+                stack.pop();
+            span[i] = stack.isEmpty() ? i + 1 : (i - stack.peek());
+            stack.push(i);
+        }
+
+        System.out.println(Arrays.toString(span));
+    }
+}
