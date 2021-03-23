bf30bf19f13739ce5a1959d63fda5de11b5257b3
Shivam Sharma <shivi08567@gmail.com>
2021-03-23 16:31:51 +0530

    Create Flood Fill

---
+++ Array/Flood Fill.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if(image[sr][sc] == newColor) return image;
+        dfs(image, sr, sc, image[sr][sc], newColor);
+        return image;
+    }
+    
+    private void dfs(int[][] image, int row, int col, int oldColor, int newColor){
+        if(row < 0 || row >= image.length || col < 0 || col >= image[0].length || image[row][col] != oldColor) return;
+        
+        image[row][col] = newColor;
+        
+        dfs(image, row + 1, col, oldColor, newColor);
+        dfs(image, row - 1, col, oldColor, newColor);
+        dfs(image, row, col + 1, oldColor, newColor);
+        dfs(image, row, col - 1, oldColor, newColor);
+    }
+}
