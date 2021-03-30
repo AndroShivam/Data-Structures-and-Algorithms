2a62e68e40e870bf6f247b75efabc217ed1e6189
Shivam Sharma <shivi08567@gmail.com>
2021-03-30 19:12:07 +0530

    Create Ugly Number

---
+++ Maths/Ugly Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n <= 0) return false;
+        
+        int[] divisors = {2,3,5};
+        
+        for(int divisor : divisors){
+            while(n % divisor == 0){
+                n /= divisor;
+            }
+        }
+        
+        return n == 1;
+    }
+}
