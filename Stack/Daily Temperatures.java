3a0a8dbd0df2afb56ffde077f3dcd7972671ad39
Shivam Sharma <shivi08567@gmail.com>
2021-04-04 11:08:47 +0530

    Update Daily Temperatures

--- Stack/Daily Temperatures.java
+++ Stack/Daily Temperatures.java
@@ -1,9 +1,36 @@
+// Brute Force 
+
+// Time Complexity - O(N^2)
+// Space Complexity - O(1)
+
 class Solution {
     public int[] dailyTemperatures(int[] T) {
         
         int n = T.length;
         int[] result = new int[n];
+        for(int i=0; i<n; i++){
+            for(int j = i + 1; j<n; j++){
+                if(T[i] < T[j]){
+                    result[i] = j - i;
+                    break;
+                }
+            }
+        }    
+        return result;
+    }
+}
+
+// Using Stack
+
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
+class Solution {
+    public int[] dailyTemperatures(int[] T) {
+        
         Stack<Integer> stack = new Stack<>();
+        int n = T.length;
+        int[] result = new int[n];
         
         for(int i=0; i<n; i++){
             while(!stack.isEmpty() && T[i] > T[stack.peek()]){
