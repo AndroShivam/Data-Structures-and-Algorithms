c3e2a9ef7242ac27340bfcb10b106bd6bae5466d
Shivam Sharma <shivi08567@gmail.com>
2021-04-05 11:56:24 +0530

    Create Valid Palindrome

---
+++ String/Valid Palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.isEmpty()) return true;
+        
+        int n = s.length() - 1;
+        
+        int start = 0;
+        int end = s.length() - 1;
+        
+        while(start < end){
+            while(start < end && !Character.isLetterOrDigit(s.charAt(start)))
+                start++;
+            
+            while(start < end && !Character.isLetterOrDigit(s.charAt(end)))
+                end--;
+            
+            if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end)))
+                return false;
+            
+            start++;
+            end--;
+        }
+        
+        return true;
+    }
+}
