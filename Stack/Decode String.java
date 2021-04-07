e991fe0f7d3b9aa6c542262bf52bc72ada98334a
Shivam Sharma <shivi08567@gmail.com>
2021-04-07 09:48:40 +0530

    Update Decode String

--- Stack/Decode String.java
+++ Stack/Decode String.java
@@ -1,29 +1,38 @@
+// Time Complexity - O(maxK*n) where maxK is max value of k & n is the size of string array.
+// Space Complexity - O(m+n) m is no of letters and n is no of digits.
+
 class Solution {
     public String decodeString(String s) {
         
         Stack<Integer> intStack = new Stack<>();
         Stack<StringBuilder> strStack = new Stack<>();
-        StringBuilder current = new StringBuilder();
+        StringBuilder curr = new StringBuilder();
+        
         int k = 0;
         
         for(char ch : s.toCharArray()){
-            if(Character.isDigit(ch))
+            if(Character.isDigit(ch)) 
                 k = k * 10 + ch - '0';
             else if(ch == '['){
                 intStack.push(k);
-                strStack.push(current);
-                current = new StringBuilder();
+                strStack.push(curr);
+                curr = new StringBuilder();
                 k = 0;
             }else if(ch == ']'){
-                StringBuilder temp =  current;
-                current = strStack.pop();
-                
-                for(int i=intStack.pop(); i>0; i--)
-                    current.append(temp);
-            }else 
-                current.append(ch);
+                StringBuilder temp = curr;
+                curr = strStack.pop();
+                for(int i = intStack.pop(); i>0; i--)
+                    curr.append(temp);
+            }else{
+                curr.append(ch);
+            }
         }
         
-        return current.toString();
+        return curr.toString();
     }
 }
+
+// Explanation for k = k * 10 + ch - '0'
+// for 23[a], in first iteration 2 is added to k
+// in second iteration k = 2 * 10 + 3 hence k = 20 + 3 = 23 
+// -'0' for char to int conversion
