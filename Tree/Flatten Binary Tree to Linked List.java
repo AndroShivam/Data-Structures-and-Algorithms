464003c1b4a4d3c157f63e5330f0234b26d186c8
Shivam Sharma <shivi08567@gmail.com>
2021-04-30 17:57:07 +0530

    Update Flatten Binary Tree to Linked List

--- Tree/Flatten Binary Tree to Linked List.java
+++ Tree/Flatten Binary Tree to Linked List.java
@@ -1,3 +1,33 @@
+// Time Complexity - O(N)
+// Space Complexity - O(N)
+
+class Solution {
+    public void flatten(TreeNode root) {
+        if(root == null) return;
+        
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        TreeNode current = null, pre = null;
+        
+        while(!stack.isEmpty()){
+            current = stack.pop();
+            if(pre != null){
+                pre.right = current;
+                pre.left = null;
+            }
+            
+            if(current.right != null) stack.push(current.right);
+            if(current.left != null) stack.push(current.left);
+            pre = current;
+        }
+        
+        current.right = null;
+    }
+}
+
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
 class Solution {
     private TreeNode prev = null;
     public void flatten(TreeNode root) {
