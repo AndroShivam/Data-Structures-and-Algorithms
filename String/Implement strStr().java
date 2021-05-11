0599d2aa28d870a292e1ac413b9bd6def1ad9300
AndroShivam <shivi08567@gmail.com>
2021-05-11 10:32:00 +0530

    Create Implement strStr()

---
+++ String/Implement strStr().java
@@ -0,0 +1,22 @@
+// Time Complexity - O(N * H)
+// Space Complexity - O(1)
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length() == 0) return 0;
+        if(haystack.length() == 0) return -1;
+        
+        for(int i=0; i<haystack.length(); i++){
+            if(i + needle.length() > haystack.length())
+                break;
+            for(int j=0; j<needle.length(); j++){
+                if(haystack.charAt(i + j) != needle.charAt(j))
+                    break;
+                if(j == needle.length() - 1)
+                    return i;
+            }
+        }
+        
+        return -1;
+    }
+}
