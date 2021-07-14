ef91fdf1e47306f66edb2a60a65a3b65c95d5072
AndroShivam <shivi08567@gmail.com>
2021-07-14 10:30:47 +0530

    Create Power of Four

---
+++ Maths/Power of Four.java
@@ -0,0 +1,8 @@
+// Time Complexity - O(1)
+// Space Complexity - O(1)
+
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && (n - 1) % 3 == 0;
+    }
+}
